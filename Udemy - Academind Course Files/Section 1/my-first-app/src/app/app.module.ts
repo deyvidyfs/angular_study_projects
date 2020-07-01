0f20181d49fa7d102cb066e97b03f6e3a1de8f1c
Deyvidy F.S <deyvidyfs@gmail.com>
2020-07-01 10:01:16 -0300

    assignment 2 dev

--- Udemy - Academind Course Files/Section 1/my-first-app/src/app/app.module.ts
+++ Udemy - Academind Course Files/Section 1/my-first-app/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { UsernameComponent } from './username/username.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { SuccessAlertComponent } from './success-alert/success-alert.component';
     ServersComponent,
     WarningAlertComponent,
     SuccessAlertComponent,
+    UsernameComponent,
   ],
   imports: [
     BrowserModule,
---
+++ Udemy - Academind Course Files/Section 1/my-first-app/src/app/username/username.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-username',
+  templateUrl: './username.component.html',
+  styleUrls: ['./username.component.css']
+})
+export class UsernameComponent implements OnInit {
+  username = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addUserName(){
+    this.username = '';
+  }
+
+}
