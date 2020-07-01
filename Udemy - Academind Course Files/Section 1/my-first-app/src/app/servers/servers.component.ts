81c7029da3ab61fff101357da438adc7303775c2
Deyvidy F.S <deyvidyfs@gmail.com>
2020-06-30 21:31:12 -0300

    added two way data binding

--- Udemy - Academind Course Files/Section 1/my-first-app/src/app/servers/servers.component.ts
+++ Udemy - Academind Course Files/Section 1/my-first-app/src/app/servers/servers.component.ts
@@ -9,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
-  serverName = '';
+  serverName = 'This is a placeholder text';
 
   constructor() { 
     setTimeout(() => {
