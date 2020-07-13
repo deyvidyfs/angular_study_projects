8a08aa1d5011e5cae371b998cf8014f6d28723ce
Deyvidy F.S <deyvidyfs@gmail.com>
2020-07-13 17:39:20 -0300

    added dynamic list for recipe items

--- Udemy - Academind Course Files/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Udemy - Academind Course Files/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', "This is simply a test", "https://img.taste.com.au/i6uNNUvx/taste/2018/02/mar-18_creamy-fettuccine-with-chicken-3000x2000-135690-1.jpg")
+  ];
 
   constructor() { }
 
